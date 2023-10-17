05c5b9103e8ebb8a7843fc1eeaa3781a4df5ccd6
nayeli <nayeligomezmoyano@gmail.com>
2023-10-17 09:45:10 +0200

    Añadida la clase HolaMundo

---
+++ lib/HolaMundo.php
@@ -0,0 +1,17 @@
+<?php
+
+class HolaMundo
+{
+   private $nombre;
+
+   function __construct($nombre)
+   {
+      $this->nombre = $nombre;
+   }
+
+   function __toString()
+   {
+      return sprintf ("Hola, %s.\n", $this->nombre);
+   }
+}
+?>
