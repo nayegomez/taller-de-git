bd5f6a59d8a7c7254549eda647da169343037923
nayeli <nayeligomezmoyano@gmail.com>
2023-10-17 09:45:37 +0200

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
 <?php
 // El autor es: Náyeli <nayeligomezmoyano@gmail.com>
 // El nombre por defecto es mundo
+require('HolaMundo.php');
+
 $nombre = isset($rgv[1]) ? $argv[1] : "Mundo";
-@print "Hola {$argv[1]}\n"; // el @ es para que no tire errores en la web
+@print new HolaMundo($nombre);
 ?>
